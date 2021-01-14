bd1cad63e97af02dcd6530635b15e3b5b92927dd
paulass97 <paula.skurczynska@gmail.com>
2021-01-14 12:02:47 +0000

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -14,8 +14,8 @@ class Tagliatelle
 	public:
 	Tagliatelle(double dlugosc, double szerokosc, double proporcje):L(dlugosc),W(szerokosc),R(proporcje){}
 	Tagliatelle():L(0.5),W(0.5),R(0.5){}
-	double ileMaki(unsigned P) const {return P*L*W*(1.-R)*C;
-	}
+	double ileMaki(unsigned P) const {return P*L*W*(1.-R)*C;}
+	
 
 	private:
 	double L;
