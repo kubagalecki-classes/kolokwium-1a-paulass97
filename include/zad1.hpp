5ef795e057dd486f87ec1a57eb4297ca814ccabd
paulass97 <paula.skurczynska@gmail.com>
2021-01-14 11:59:23 +0000

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,8 +1,25 @@
 #include "catch.hpp"
+#inlcude <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
 
 // Zad2
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 
-// Zad1
-// tutaj definicja klasy Tagliatelle
+class Tagliatelle
+{
+	public:
+	Tagliatelle(double dlugosc, double szerokosc, double proporcje):L(dlugosc),W(szerokosc),R(proporcje){}
+	Tagliatelle():L(0.5),W(0.5),R(0.5){}
+	double ileMaki(unsigned P) const {return P*L*W*(1.-R)*C;
+	}
+
+	private:
+	double L;
+	double W;
+	double R;
+	static const double C;
+};
--- src/tests_main.cpp
+++ src/tests_main.cpp
@@ -1,2 +1,23 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include <iostream>
+#include <algorithm>
+#include <string>
+'run="echo hello word"'
+
+using namespace std;
+
+
+class Tagliatelle{
+	public:
+	Tagliatelle(double dlugosc,double szerokosc,double proporcje):L(dlugosc),W(szerokosc),R(proporcje){}
+	Tagliatelle():L(0.5),W(0.5),R(0.5){}
+	double ileMaki(unsigned P)const{return P*L*W*(1.-R)*C;}
+	private:
+	double L;
+	double W;
+	double R;
+	static const double C;
+};
+
+int main(){}
