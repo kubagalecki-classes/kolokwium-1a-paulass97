cda4935a4db26f49d4f7330e829790fb44451e56
paulass97 <paula.skurczynska@gmail.com>
2021-01-14 12:36:36 +0000

    zassss

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,29 +1,23 @@
 #include "catch.hpp"
-#include <iostream>
-#include <algorithm>
-#include <string>
 
 using namespace std;
 
-// Zad2
-// tutaj definicja klasy Makaron
-// BEZ definicji metody gotujMakaron (tylko z deklaracją)
-
-class Tagliatelle
+class Tagliatelle : 
 {
-	public:
-	Tagliatelle():L(0.5),W(0.5),R(0.5){}
-	Tagliatelle(double dlugosc, double szerokosc, double proporcje):L(dlugosc),W(szerokosc),R(proporcje){}
+  public:
+  Tagliatelle(): L(0.5), W(0.5), R(0.5) {}
+  Tagliatelle(double l, double w, double r): L(l), W(w), R(r) {}
+  
+  double ileMaki(unsigned P) const
+  {
+    return P * L * W * (1. - R) * C;
+  }
 
-	double ileMaki(unsigned P)const{return P*L*W*(1.-R)*C;}
-	
-
-	private:
-	double L;
-	double W;
-	double R;
-	static const double C;
+  private:
+  double L;
+  double W;
+  double R;
+  
+  static const double C;
 };
-
-
 
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -1,5 +1,4 @@
 #include "zad1.hpp"
-
 #include <memory>
 #include <string>
 #include <type_traits>
